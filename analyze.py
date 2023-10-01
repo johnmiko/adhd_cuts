5546a53563875a28752724d38a510bc1c370820b
johnmiko <johnmiko4@gmail.com>
2023-10-01 15:04:54 -0400

    join clips

--- analyze.py
+++ analyze.py
@@ -4,7 +4,7 @@ How to automatically cut video, given the timestamps
 """
 import pandas as pd
 
-df = pd.read_csv('timestamps2.csv', header=2)
+df = pd.read_csv('timestamps4.csv', header=2)
 # Fill Nans with start of next, or end of previous
 df.end = df.end.fillna(df.start.shift(-1))
 df.start = df.start.fillna(df.end.shift(1))
@@ -14,14 +14,14 @@ def to_seconds(row, column):
     timestamp_raw = row[column]
     timestamp_parts_str = timestamp_raw.split(':')
     print(timestamp_parts_str)
-    timestamp_parts = [int(part) for part in timestamp_parts_str]
+    timestamp_parts = [float(part) for part in timestamp_parts_str]
     # hour, minute, seconds,
     if len(timestamp_parts) == 3:
         # # excel added stupid 00 to the timestamp
-        if (timestamp_parts_str[-1] == "00") and (timestamp_parts_str[0] != "1"):
-            seconds = timestamp_parts[0] * 60 + timestamp_parts[1]
-        else:
-            seconds = timestamp_parts[0] * 3600 + timestamp_parts[1] * 60 + timestamp_parts[2]
+        # if (timestamp_parts_str[-1] == "00") and (timestamp_parts_str[0] != "1"):
+        #     seconds = timestamp_parts[0] * 60 + timestamp_parts[1]
+        # else:
+        seconds = timestamp_parts[0] * 3600 + timestamp_parts[1] * 60 + timestamp_parts[2]
     elif len(timestamp_parts) == 2:
         seconds = timestamp_parts[0] * 60 + timestamp_parts[1]
     else:
@@ -37,7 +37,9 @@ group = df.groupby("needed")
 run_times = group.sum("duration (s)")
 run_times["duration (m)"] = (run_times["duration (s)"] / 60).round(0)
 print(run_times)
-"""
-run times
-add up run times based on needed
-"""
+# clips = []
+# filename = f'temp.mp4'
+# clips.append(VideoFileClip("The.Other.Guys.1080p - original.mp4").subclip(194.5, 218))
+# result = concatenate_videoclips(clips)
+# result.write_videofile('the other guys - adhd cut v2.mp4')
+# VideoFileClip("The.Other.Guys.1080p - original.mp4").subclip(194.5, 218).write_videofile('temp.mp4')
--- join_clips.py
+++ join_clips.py
@@ -9,7 +9,7 @@ from moviepy.video.io.VideoFileClip import VideoFileClip
 
 from analyze import to_seconds
 
-df = pd.read_csv('timestamps3.csv', header=2)
+df = pd.read_csv('timestamps4.csv', header=2)
 # Fill Nans with start of next, or end of previous
 df.end = df.end.fillna(df.start.shift(-1))
 df.start = df.start.fillna(df.end.shift(1))
@@ -35,5 +35,7 @@ for i, row in df.iterrows():
         filename = f'clips/{row["start_seconds"]}_{row["end_seconds"]}.mp4'
         clips.append(VideoFileClip("The.Other.Guys.1080p - original.mp4").subclip(row["start_seconds"],
                                                                                   row["end_seconds"]))
+        if row["mute"] == "yes":
+            clips[-1] = clips[-1].without_audio()
 result = concatenate_videoclips(clips)
-result.write_videofile('the other guys - adhd cut v2.mp4')
+result.write_videofile('the other guys - adhd cut v3.mp4')
